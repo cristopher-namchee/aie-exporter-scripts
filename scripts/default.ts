5eb904b9f252f23f2e4b8b4fe37fc0566cf15bad
Cristopher <cristopher@gdplabs.id>
2023-11-13 12:39:29 +0700

    feat: add default exporter script

--- scripts/default.ts
+++ scripts/default.ts
@@ -1,15 +1,26 @@
 import * as excel from "exceljs";
 
-import { OCRResponse } from "../types";
+import { OCRField, OCRResponse } from "../types";
 
 export default async function exportToSheet(
-  _: OCRResponse,
+  response: OCRResponse,
   documentName: string
 ): Promise<Buffer> {
   const workbook = new excel.Workbook();
+  // truncate limit
+  documentName = documentName.slice(0, 31);
+
   const sheet = workbook.addWorksheet(documentName);
 
-  const rows = [];
+  const rows: string[][] = [];
+  const read = response.read;
+  if (!Array.isArray(read)) {
+    for (const [key, value] of Object.entries(read)) {
+      const val = value as OCRField;
+
+      rows.push([key, JSON.stringify(value)]);
+    }
+  }
 
   sheet.addRows(rows);
 
